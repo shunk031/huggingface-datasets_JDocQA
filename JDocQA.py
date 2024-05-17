f7806d73807e458bca34ed17d1d53ce0fd003a70
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-05-17 15:30:41 +0900

    update script

--- JDocQA.py
+++ JDocQA.py
@@ -17,6 +17,7 @@
 # TODO: Address all TODOs and remove all explanatory comments
 import json
 import os
+import re
 from typing import List
 
 import datasets as ds
@@ -24,27 +25,29 @@ from datasets.utils.logging import get_logger
 
 logger = get_logger(__name__)
 
-# TODO: Add BibTeX citation
-# Find for instance the citation on arxiv or on the dataset repo/website
 _CITATION = """\
-TODO: Add BibTeX citation here
+@inproceedings{JDocQA_2024,
+    title = "JDocQA: Japanese Document Question Answering Dataset for Generative Language Models",
+    author = "Onami, Eri  and
+      Kurita, Shuhei  and
+      Miyanishi, Taiki and
+      Watanabe, Taro",
+    booktitle = "The 2024 Joint International Conference on Computational Linguistics, Language Resources and Evaluation",
+    month = may,
+    year = "2024",
+    address = "Trino, Italy",
+    abstract = "Document question answering is a task of question answering on given documents such as reports, slides, pamphlets, and websites, and it is a truly demanding task as paper and electronic forms of documents are so common in our society. This is known as a quite challenging task because it requires not only text understanding but also understanding of figures and tables, and hence visual question answering (VQA) methods are often examined in addition to textual approaches. We introduce Japanese Document Question Answering (JDocQA), a large-scale document-based QA dataset, essentially requiring both visual and textual information to answer questions, which comprises 5,504 documents in PDF format and annotated 11,600 question-and-answer instances in Japanese. Each QA instance includes references to the document pages and bounding boxes for the answer clues. We incorporate multiple categories of questions and unanswerable questions from the document for realistic question-answering applications. We empirically evaluate the effectiveness of our dataset with text-based large language models (LLMs) and multimodal models. Incorporating unanswerable questions in finetuning may contribute to harnessing the so-called hallucination generation.",
+}
 """
 
-# TODO: Add description of the dataset here
-# You can copy an official description
 _DESCRIPTION = """\
 Japanese Document Question Answering (JDocQA), a large-scale document-based QA dataset, essentially requiring both visual and textual information to answer questions, which comprises 5,504 documents in PDF format and annotated 11,600 question-and-answer instances in Japanese.
 """
 
-# TODO: Add a link to an official homepage for the dataset here
 _HOMEPAGE = "https://github.com/mizuumi/JDocQA"
 
-# TODO: Add the licence for the dataset here if you can find it
 _LICENSE = "JDocQA dataset annotations are distributed under CC BY-SA 4.0."
 
-# TODO: Add link to the official dataset URLs here
-# The HuggingFace Datasets library doesn't host the datasets but only points to the original files.
-# This can be an arbitrary nested dict/list of URLs (see below in `_split_generators` method)
 _URLS = {
     "annotations": {
         "train": "https://raw.githubusercontent.com/mizuumi/JDocQA/main/dataset/annotation_files/jdocqa_train_all.json",
@@ -55,7 +58,6 @@ _URLS = {
 }
 
 
-# TODO: Name of the dataset usually matches the script name with CamelCase instead of snake_case
 class JDocQADataset(ds.GeneratorBasedBuilder):
     """A class for loading JDocQA dataset."""
 
@@ -72,26 +74,54 @@ class JDocQADataset(ds.GeneratorBasedBuilder):
         features = ds.Features(
             {
                 "answer": ds.Value("string"),
-                "answer_type": ds.Value("string"),
+                "answer_type": ds.ClassLabel(
+                    num_classes=4,
+                    names=["yes/no", "factoid", "numerical", "open-ended"],
+                ),
                 "context": ds.Value("string"),
-                "multiple_select_answer": ds.Value("string"),
-                "multiple_select_question": ds.Value("string"),
-                "no_reason": ds.Value("string"),
+                "multiple_select_answer": ds.ClassLabel(
+                    num_classes=4,
+                    names=["A", "B", "C", "D"],
+                ),
+                "multiple_select_question": ds.Sequence(ds.Value("string")),
+                "no_reason": ds.ClassLabel(
+                    num_classes=4,
+                    names=["0", "1", "2", "1,2"],
+                ),
                 "normalized_answer": ds.Value("string"),
                 "original_answer": ds.Value("string"),
                 "original_context": ds.Value("string"),
                 "original_question": ds.Value("string"),
-                "pdf_category": ds.Value("string"),
+                "pdf_category": ds.ClassLabel(
+                    num_classes=4,
+                    names=["Document", "Kouhou", "Slide", "Website"],
+                ),
                 "pdf_name": ds.Value("string"),
                 "question": ds.Value("string"),
-                "question_number": ds.Value("string"),
+                "question_number": ds.Sequence(ds.Value("uint64")),
                 "question_page_number": ds.Value("string"),
-                "reason_of_answer_bbox": ds.Value("string"),
+                "reason_of_answer_bbox": ds.Sequence(ds.Value("string")),
                 "text_from_ocr_pdf": ds.Value("string"),
                 "text_from_pdf": ds.Value("string"),
-                "type_of_image": ds.Value("string"),
+                "type_of_image": ds.Sequence(
+                    ds.ClassLabel(
+                        num_classes=10,
+                        names=[
+                            "Null",
+                            "Table",
+                            "Bar chart",
+                            "Line chart",
+                            "Pie chart",
+                            "Map",
+                            "Other figures",
+                            "Mixtured writing style from left to the right and from upside to the downside",
+                            "Drawings",
+                            "Others",
+                        ],
+                    )
+                ),
                 #
-                # 'pdf_filepath' is added to the original dataset for convenience
+                # `pdf_filepath` is added to the original dataset for convenience
                 "pdf_filepath": ds.Value("string"),
             }
         )
@@ -138,13 +168,107 @@ class JDocQADataset(ds.GeneratorBasedBuilder):
             ),
         ]
 
+    def _convert_answer_type(self, answer_type: str) -> str:
+        if answer_type == "1":
+            return "yes/no"
+        elif answer_type == "2":
+            return "factoid"
+        elif answer_type == "3":
+            return "numerical"
+        elif answer_type == "4":
+            return "open-ended"
+        else:
+            raise ValueError(f"Unknown answer type: {answer_type}")
+
+    def _convert_multiple_select_question(
+        self, multiple_select_question: str
+    ) -> List[str]:
+        _, qs = multiple_select_question.split("(A)")
+
+        questions = []
+        for sep in ("(B)", "(C)", "(D)"):
+            q, qs = qs.split(sep)
+            questions.append(q)
+        questions.append(qs)
+
+        assert (
+            len(questions) == 4
+        ), f"Before: {multiple_select_question}, After: {questions}"
+
+        questions = [question.rstrip("、") for question in questions]
+        return questions
+
+    def _convert_question_number(self, question_number: str) -> List[int]:
+        return [int(qn) for qn in question_number.split("-")]
+
+    def _convert_reason_of_answer_bbox(self, reason_of_answer_bbox: str) -> List[str]:
+        reason_of_answer_bboxes = [
+            r for r in re.split(r"[.,、､]", reason_of_answer_bbox)
+        ]
+        check = [r.isdigit() if r != "" else r == "" for r in reason_of_answer_bboxes]
+        assert all(check), reason_of_answer_bboxes
+        return reason_of_answer_bboxes
+
+    def _convert_type_of_image(self, type_of_image: str) -> List[str]:
+        types_of_image = type_of_image.split(",")
+
+        def convert_to_type_of_image(type_of_image: str) -> str:
+            if type_of_image == "":
+                return "Null"
+            elif type_of_image == "1":
+                return "Table"
+            elif type_of_image == "2":
+                return "Bar chart"
+            elif type_of_image == "3":
+                return "Line chart"
+            elif type_of_image == "4":
+                return "Pie chart"
+            elif type_of_image == "5":
+                return "Map"
+            elif type_of_image == "6":
+                return "Other figures"
+            elif type_of_image == "7":
+                return "Mixtured writing style from left to the right and from upside to the downside"
+            elif type_of_image == "8":
+                return "Drawings"
+            elif type_of_image == "9":
+                return "Others"
+            else:
+                raise ValueError(f"Unknown type of image: {type_of_image}")
+
+        return [convert_to_type_of_image(t) for t in types_of_image]
+
+    def _get_pdf_fielpath(self, pdf_name: str, documents_dir: str) -> str:
+        pdf_filepath = os.path.join(documents_dir, pdf_name)
+        assert os.path.exists(pdf_filepath), f"File not found: {pdf_filepath}"
+        return pdf_filepath
+
     # method parameters are unpacked from `gen_kwargs` as given in `_split_generators`
     def _generate_examples(self, annotation_path: str, documents_dir: str):
         with open(annotation_path) as rf:
             for i, line in enumerate(rf):
                 data = json.loads(line)
-                pdf_filepath = os.path.join(documents_dir, data["pdf_name"])
-                assert os.path.exists(pdf_filepath), f"File not found: {pdf_filepath}"
 
-                data["pdf_filepath"] = pdf_filepath
+                data["answer_type"] = self._convert_answer_type(
+                    answer_type=data["answer_type"]
+                )
+                data["multiple_select_question"] = (
+                    self._convert_multiple_select_question(
+                        multiple_select_question=data["multiple_select_question"]
+                    )
+                )
+                data["question_number"] = self._convert_question_number(
+                    data["question_number"]
+                )
+                data["reason_of_answer_bbox"] = self._convert_reason_of_answer_bbox(
+                    data["reason_of_answer_bbox"]
+                )
+                data["type_of_image"] = self._convert_type_of_image(
+                    type_of_image=data["type_of_image"]
+                )
+                data["pdf_filepath"] = self._get_pdf_fielpath(
+                    pdf_name=data["pdf_name"],
+                    documents_dir=documents_dir,
+                )
+
                 yield i, data
